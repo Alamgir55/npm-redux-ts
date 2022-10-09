1b2884a003ca393696e8fa918b31fe3a07fb3c30
Alamgir55 <alamgir555hossain@gmail.com>
2022-10-09 00:33:15 -0400

    'ts-redux-setup-reducer-actions'

---
+++ src/state/action-types/index.ts
@@ -0,0 +1,5 @@
+export enum ActionType {
+  SEARCH_RESPOSITORIES = "search_respositories",
+  SEARCH_RESPOSITORIES_SUCCESS = "search_respositories_success",
+  SEARCH_RESPOSITORIES_ERROR = "search_respositories_error",
+}
